ea98f636e49b2552f1c1d9ad8c15b44e32e601ec
Skyler <skylerloveuk@gmail.com>
2024-11-18 13:16:17 +0800

    code render

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,16 @@
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
